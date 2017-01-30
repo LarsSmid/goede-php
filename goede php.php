15efd615f4ad7ceef07bb7bbd95d7feef5a37483
LAPTOP-LARS\Gebruiker <99039541@mydavinci.nl>
2017-01-30 12:46:25 +0100

    7.0

--- goede php.php
+++ goede php.php
@@ -1,30 +1,33 @@
 <?php
+    date_default_timezone_set('Europe/Amsterdam');
     
-    $bg = "morning.png";
     $time = date("H");
 
-    if ($time >= 6 && $time < 12){
+    if ($time >= 6 && $time < 11){
         $bg = "morning.png";
-        $greeting = "";
-    } elseif ($time >= 12 && $time < 18) {
+        $greeting = "goedemorgen";
+    } elseif ($time >= 11 && $time < 17) {
         $bg = "afternoon.png";
-    } elseif ($time >= 18 && $time == 23 ) {
+        $greeting = "goedemiddag";
+    } elseif ($time >= 17 && $time < 23 ) {
         $bg = "evening.png";
+        $greeting = "goedeavond";
     } elseif ($time >= 0 && $time < 6) {
         $bg = "night.png";
+        $greeting = "goedenacht";
     }
-
+  
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>goede php</title>
-	<link rel="stylesheet" type="text/css" href="opmaak.css">
+	<link rel="stylesheet" type="text/css" href="test.css">
 </head>
 
 <body style="background: url('bg/<?php echo $bg ?>'); background-size: cover;">
-
-
+<h1> <?php echo $greeting; ?> </h1>
+<p> <?php echo "Het is nu " . date("H:i"); ?> </p>
 </body>
 </html>
